dc7d5cb6fd52f6fa6ea106780345d9787bac0ab1
Mariana <75488872+marianapequeno@users.noreply.github.com>
2023-07-27 15:11:11 -0300

    Exercícios Lógica Estrutra Condicional 7 e 8

---
+++ 2-Estrutura_condicional/07_ExercicioCoordenadas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioCoordenadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTRADA: ");
+            string[] coordenadas = Console.ReadLine().Split(' ');
+            double X = double.Parse(coordenadas[0], CultureInfo.InvariantCulture);
+            double Y = double.Parse(coordenadas[1], CultureInfo.InvariantCulture);
+
+            if (X > 0 && Y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (X < 0 && Y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (X < 0 && Y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (X > 0 && Y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else
+            {
+                Console.WriteLine("Origem");
+            }
+        }
+    }
+}
